b9a833c6a44eadbc981c7da092dee1b8aafb917b
Alejandra2431 <maria.sosadias@correo.ucu.edu.uy>
2021-09-15 17:22:58 -0300

    Elf terminado

--- src/Library/Elf.cs
+++ src/Library/Elf.cs
@@ -1,39 +1,81 @@
+using System;
 namespace Library
 {
     public class Elf
     {
-        private string Name {get; set;}
+        private string name {get; set;}
         private int health  {get; set;}
         private int attack {get; set;}
+
+        private int armor {get; set;}  
+        private List<Item> inventory;
     
         public Elf (string name)
         {
-            this.Name = name;
-            this.attack = 25;
+            this.name = name;
+            this.health = 100;
+            this.attack = 35;
+            this.armor = 5;
+            this.inventory = new List<Item> {};
 
         }
         public void AttackEnemy(Knight enemyToAtack)
         {
-            enemyToAtack.health -= this.attack;
-            Console.WriteLine($"{this.name} ataca a {enemyToAtack.name}")
-            Console.WriteLine($"Se le quita {this.attack} puntos de vida a {enemyToAtack.name}")
+            enemyToAtack.health = enemyToAtack.health - (this.attack - (this.attack * (enemyToAtack.armor / 100)));
+            Console.WriteLine($"{this.name} ataca a {enemyToAtack.name}");
+            Console.WriteLine($"Se le quita {this.attack} puntos de vida a {enemyToAtack.name}");
         }
 
         public void AttackEnemy(Wizard enemyToAtack)
         {
-            enemyToAtack.health -= this.attack;
-            Console.WriteLine($"{this.name} ataca a {enemyToAtack.name}")
-            Console.WriteLine($"Se le quita {this.attack} puntos de vida a {enemyToAtack.name}")
+            enemyToAtack.health = enemyToAtack.health - (this.attack - (this.attack * (enemyToAtack.armor / 100)));
+            Console.WriteLine($"{this.name} ataca a {enemyToAtack.name}");
+            Console.WriteLine($"Se le quita {this.attack} puntos de vida a {enemyToAtack.name}");
         }
 
         public void AttackEnemy(Dwarf enemyToAtack)
         {
-            enemyToAtack.health -= this.attack;
-            Console.WriteLine($"{this.name} ataca a {enemyToAtack.name}")
-            Console.WriteLine($"Se le quita {this.attack} puntos de vida a {enemyToAtack.name}")
+            
+            enemyToAtack.health = enemyToAtack.health - (this.attack - (this.attack * (enemyToAtack.armor / 100)));
+            Console.WriteLine($"{this.name} ataca a {enemyToAtack.name}");
+            Console.WriteLine($"Se le quita {this.attack} puntos de vida a {enemyToAtack.name}");
+        }
+        public void AttackEnemy(Elf enemyToAtack)
+        {
+            enemyToAtack.health = enemyToAtack.health - (this.attack - (this.attack * (enemyToAtack.armor / 100)));
+            Console.WriteLine($"{this.name} ataca a {enemyToAtack.name}");
+            Console.WriteLine($"Se le quita {this.attack} puntos de vida a {enemyToAtack.name}");
         }
 
-        // se pueden defender? la defensa se le suma en los métodos desde
-        // donde lo atacan (clase del personaje que ataca) para no restar tanta vida?
+        public void EquipItem(Item item)
+        {
+            this.inventory.Add(item);
+            this.attack = this.attack + item.ReturnDamage();
+            this.armor = this.armor + item.ReturnArmor();
+        }
+
+        public void UnequipItem(Item item)
+        {
+            this.attack = this.attack - item.ReturnDamage();
+            this.armor = this.armor - item.ReturnArmor();
+            this.inventory.Remove(item);
+        }
+        
+        public int ReturnArmor()
+        {
+            return this.armor;
+        }
+        public int ReturnAttack()
+        {
+            return this.attack;
+        }
+        public int CurrentHealth()
+        {
+            return this.health;
+        }
+        public void RestoreHealth()
+        {
+            this.health = 100;
+        }
     }
 }
