c8aa170946169958faed4bba92c6ee0b8190ecb5
Alejandra2431 <maria.sosadias@correo.ucu.edu.uy>
2021-09-17 16:14:10 -0300

    Pelea de elfos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Library;
 
 namespace Program
 {
@@ -6,6 +7,18 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            Elf elf1 = new Elf("Elf1");
+            Elf elf2 = new Elf("Elf2");
+            Item item1 = new Item("Fuego", 10, 0);
+            Item item2 = new Item("Escudo", 10, 5);
+            elf1.EquipItem(item1);
+            elf1.EquipItem(item2);
+            
+            while (elf2.CurrentHealth()>0)
+            {
+                elf1.AttackElf(elf2);
+                elf2.AttackElf(elf1);
+            }
         }
     }
 }
