049abdbb1b501a9914191cd91732651df8d08fb5
Alejandra2431 <maria.sosadias@correo.ucu.edu.uy>
2021-09-16 23:46:43 -0300

    Comentarios el clase Elf

--- src/Library/Elf.cs
+++ src/Library/Elf.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 namespace Library
 {
+    /// <summary>
+    /// Clase correspondiente al personaje Elfo.
+    /// </summary>
     public class Elf
     {
         private string name {get; set;}
@@ -9,7 +12,12 @@ namespace Library
         private int attack {get; set;}
         private int armor {get; set;}  
         private List<Item> inventory;
-    
+
+        /// <summary>
+        /// Constructor de la clase Elf, recibe por parámetro el nombre del elfo, además se le asignan valores 
+        /// a los tres atributos de la case y se inicializa la lista de elementos pertenecientes al personaje.
+        /// </summary>
+        /// <param name="name"></param>
         public Elf (string name)
         {
             this.name = name;
@@ -18,6 +26,7 @@ namespace Library
             this.armor = 5;
             this.inventory = new List<Item>();
         }
+
         public int Health
         {
             get
@@ -29,12 +38,21 @@ namespace Library
                 this.health = value;
             }
         }
-        
+
+        /// <summary>
+        /// Método que recibe por parámetro en entero que corresponde al daño que recive el elfo
+        /// y se le resta de su vida.
+        /// </summary>
+        /// <param name="damage"></param>        
         public void RecieveDamage(int damage)
         {
             this.health = this.health - damage;
         }
 
+        /// <summary>
+        /// Método con el que se ataca al knight que recibe por parámetro.
+        /// </summary>
+        /// <param name="enemyToAtack"></param>
         public void AttackKnight(Knight enemyToAtack)
         {
             Console.WriteLine($"Se ataca a {enemyToAtack.ReturnName()}.");
@@ -42,6 +60,10 @@ namespace Library
             Console.WriteLine($"-{this.attack} de vida a {enemyToAtack.ReturnName()}");
         }
 
+        /// <summary>
+        /// Método con el que se ataca al wizard que se recibe por parámetro. 
+        /// </summary>
+        /// <param name="enemyToAtack"></param>
         public void AttackWizard(Wizard enemyToAtack)
         {
             Console.WriteLine($"Se ataca a {enemyToAtack.ReturnName()}.");
@@ -49,12 +71,21 @@ namespace Library
             Console.WriteLine($"-{this.attack} de vida a {enemyToAtack.ReturnName()}");
         }
 
+        /// <summary>
+        /// Método con el que se ataca al dwarf que se recibe por parámetro.
+        /// </summary>
+        /// <param name="enemyToAtack"></param>
         public void AttackDwarf(Dwarf enemyToAtack)
         {
             Console.WriteLine($"Se ataca a {enemyToAtack.ReturnName()}.");
             enemyToAtack.RecieveDamage(this.attack);
             Console.WriteLine($"-{this.attack} de vida a {enemyToAtack.ReturnName()}");
         }
+
+        /// <summary>
+        /// Método con el que se ataca al elf que se recibe por parámetro.
+        /// </summary>
+        /// <param name="enemyToAtack"></param>
         public void AttackElf(Elf enemyToAtack)
         {
             Console.WriteLine($"Se ataca a {enemyToAtack.ReturnName()}.");
@@ -62,6 +93,11 @@ namespace Library
             Console.WriteLine($"-{this.attack} de vida a {enemyToAtack.ReturnName()}");
         }
 
+        /// <summary>
+        /// Método para agregar un item (recibido por parámetro) a la lista inventario de la clase.
+        /// También se aumenta el valor del ataque o de la defensa del elfo según cómo corresponda.
+        /// </summary>
+        /// <param name="item"></param>
         public void EquipItem(Item item)
         {
             this.inventory.Add(item);
@@ -69,6 +105,11 @@ namespace Library
             this.armor = this.armor + item.ReturnArmor();
         }
 
+        /// <summary>
+        /// Método para quitar un item (recibido por parámetro) de la lista inventario de la clase.
+        /// También se le disminuye el valor al ataque o a la defensa, según cómo corresponda.
+        /// </summary>
+        /// <param name="item"></param>
         public void UnequipItem(Item item)
         {
             this.attack = this.attack - item.ReturnDamage();
@@ -76,26 +117,45 @@ namespace Library
             this.inventory.Remove(item);
         }
         
+        /// <summary>
+        /// Método para devolver el valor de la defensa del elfo.
+        /// </summary>
+        /// <returns></returns>
         public int ReturnArmor()
         {
             return this.armor;
         }
 
+        /// <summary>
+        /// Método para devolver el valor del ataque del elfo. 
+        /// </summary>
+        /// <returns></returns>
         public int ReturnAttack()
         {
             return this.attack;
         }
 
+        /// <summary>
+        /// Método para devolver el la vida actual que tiene el elfo. 
+        /// </summary>
+        /// <returns></returns>
         public int CurrentHealth()
         {
             return this.health;
         }
 
+        /// <summary>
+        /// Método para que el elfo vuelva a tener el 100% de vida.
+        /// </summary>
         public void RestoreHealth()
         {
             this.health = 100;
         }
 
+        /// <summary>
+        /// Método que devuelve el inventario 
+        /// </summary>
+        /// <returns></returns>
         public List<Item> ReturnInventory()
         {
             return this.inventory;
